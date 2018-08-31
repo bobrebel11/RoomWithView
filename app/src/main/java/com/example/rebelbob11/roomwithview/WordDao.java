821cb349d23446ee84d73f1c81b8c50e9df883e3
rebelbob11 <Hunter.11>
2018-08-31 12:07:42 -0400

    Added Word DAO(Data Access Object)

---
+++ app/src/main/java/com/example/rebelbob11/roomwithview/WordDao.java
@@ -0,0 +1,21 @@
+package com.example.rebelbob11.roomwithview;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface WordDao {
+
+    @Insert
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM WORD_TABLE ORDER BY word ASC")
+    List<Word> getAllWords();
+
+}
